d89e7b4a02784bcfec7b2337bc48dc3d05b0775b
Julio1099 <juliio1520@gmail.com>
2026-01-18 17:55:51 -0300

    chore(infra): setup completo - postgres, whitenoise e auto-admin

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 # exit on error
 set -o errexit
 
+# 1. Instala dependências (incluindo Gunicorn e Whitenoise)
 pip install -r requirements.txt
 
+# 2. Arruma os arquivos estáticos (CSS/Imagens do sistema)
 python manage.py collectstatic --no-input
+
+# 3. Cria as tabelas no Banco de Dados (Postgres)
 python manage.py migrate
 
 python manage.py createsuperuser --noinput || true
--- skateshop/settings.py
+++ skateshop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,3 +127,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
